6b527bfc4bd09f7be3c64c2d2d1211e1bd49fec4
dmtarua <dmtar@ua.pt>
2020-07-30 16:43:04 +0000

    added subscribe function

--- Central + Utils/ble.py
+++ Central + Utils/ble.py
@@ -1,5 +1,6 @@
 import sys
 from bluepy.btle import Scanner, DefaultDelegate, Peripheral, Characteristic, Service
+from bluepy import btle
 
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
@@ -11,7 +12,14 @@ class ScanDelegate(DefaultDelegate):
         elif isNewData:
             print ("Received new data from", dev.addr)
 
-def scanAll():
+class NotificationHandler(btle.DefaultDelegate):
+    def __init__(self):
+        btle.DefaultDelegate.__init__(self)
+
+    def handleNotification(self, cHandle, data):
+        print("A notification was received: %s" %data)
+
+def scan():
     scanner = Scanner().withDelegate(ScanDelegate())
     devices = scanner.scan(10.0)
 
@@ -19,7 +27,6 @@ def scanAll():
         print ("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
         for (adtype, desc, value) in dev.getScanData():
             print ("  %s = %s" % (desc, value))
-
 
 def getServices(mac):
     periph = Peripheral()
@@ -47,12 +54,36 @@ def getCharacteristics(mac):
     print("Disconnecting...")
     periph.disconnect()
 
-def write(mac):
+def write(mac, handle):
     periph = Peripheral()
     print("Connecting to: ", mac)
     periph.connect(mac)
 
-    periph.writeCharacteristic(14, b'xff')
+    periph.writeCharacteristic(handle, b'0000100')
+
+    print("Disconnecting...")
+    periph.disconnect()
+
+def read(mac, handle):
+    periph = Peripheral()
+    print("Connecting to: ", mac)
+    periph.connect(mac)
+
+    r = periph.readCharacteristic(handle)
+    print(r)
+
+    print("Disconnecting...")
+    periph.disconnect()
+
+def subscribe(mac):
+    periph = Peripheral()
+    print("Connecting to: ", mac)
+    periph.connect(mac)
+    periph.setDelegate(NotificationHandler())
+
+    while True:
+        if periph.waitForNotifications(1.0):
+            continue
 
     print("Disconnecting...")
     periph.disconnect()
@@ -60,10 +91,16 @@ def write(mac):
 if(len(sys.argv) < 2):
     print ("Usage: \tpython ble.py scan\nor \tpython ble.py connect [MAC]")
 elif (str(sys.argv[1]) == "scan"):
-    scanAll()
+    scan()
 elif (str(sys.argv[1]) == "services"):
     getServices("28:ed:e0:a5:b0:84")
 elif (str(sys.argv[1]) == "characteristics"):
     getCharacteristics("28:ed:e0:a5:b0:84")
 elif (str(sys.argv[1]) == "write"):
-    write("28:ed:e0:a5:b0:84")
+    write("28:ed:e0:a5:b0:84", int(sys.argv[2]))
+elif (str(sys.argv[1]) == "read"):
+    read("28:ed:e0:a5:b0:84", int(sys.argv[2]))
+elif (str(sys.argv[1]) == "subscribe"):
+    subscribe("28:ed:e0:a5:b0:84")
+else:
+    print ("Usage: \tpython ble.py scan\nor \tpython ble.py connect [MAC]")
